0c21469aef1f0871b3373789e90aa3a505c11942
Muhammad Habibulloh <119352803+muhammdhabib@users.noreply.github.com>
2023-03-20 12:41:29 +0700

    Add files via upload

---
+++ StrukturDataModul1/src/Hidangan.java
@@ -0,0 +1,15 @@
+public class Hidangan {
+    protected String NamaHidangan;
+
+    public String getNamaHidangan() {
+        return NamaHidangan;
+    }
+
+    public void setNamaHidangan(String namaHidangan) {
+        NamaHidangan = namaHidangan;
+    }
+
+    public String disantap(){
+        return "makanan dihidangkan";
+    }
+}
